70ce9f084c47638827aa92b72e2ed581f4bca36b
Choonster TheMage <Choonster.2010@gmail.com>
2016-01-25 14:44:34 +1100

    Manually add blockstate IDs to the map

--- src/main/java/com/com/choonster/forgesubstitutionaliastestmod/ForgeSubstitutionAliasTestMod.java
+++ src/main/java/com/com/choonster/forgesubstitutionaliastestmod/ForgeSubstitutionAliasTestMod.java
@@ -1,16 +1,21 @@
 package com.com.choonster.forgesubstitutionaliastestmod;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.ItemMeshDefinition;
 import net.minecraft.client.resources.model.ModelBakery;
 import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ObjectIntIdentityMap;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLModIdMappingEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.ExistingSubstitutionException;
+import net.minecraftforge.fml.common.registry.GameData;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import org.apache.logging.log4j.Logger;
 
@@ -20,12 +25,13 @@ public class ForgeSubstitutionAliasTestMod {
 	public static final String NAME = "Forge Substitution Alias Test Mod";
 	private Logger LOGGER;
 
+	private BlockSnowLayerReplacement blockSnowLayerReplacement;
+
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 		LOGGER = event.getModLog();
 
-		BlockSnowLayerReplacement blockSnowLayerReplacement = new BlockSnowLayerReplacement();
-
+		blockSnowLayerReplacement = new BlockSnowLayerReplacement();
 		try {
 			GameRegistry.addSubstitutionAlias("minecraft:snow_layer", GameRegistry.Type.BLOCK, blockSnowLayerReplacement);
 //			GameRegistry.addSubstitutionAlias("minecraft:snow_layer", GameRegistry.Type.ITEM, new ItemSnow(blockSnowLayerReplacement));
@@ -50,7 +56,29 @@ public class ForgeSubstitutionAliasTestMod {
 
 	@Mod.EventHandler
 	public void postInit(FMLPostInitializationEvent event) {
-		Block snow = GameRegistry.findBlock("minecraft", "snow_layer");
-		LOGGER.info("minecraft:snow_layer = %s", snow.getClass());
+		LOGGER.info("minecraft:snow_layer = {}", getSnowBlock().getClass());
+	}
+
+	/*
+	 This is a hack and shouldn't be needed, but it does replace any occurrences of the vanilla block in the world with the replacement block.
+	 The model renders properly, the F3 debug info displays the correct state and Block#onBlockActivated is called.
+
+	 This breaks /setblock.
+	  */
+	@Mod.EventHandler
+	public void idMapping(FMLModIdMappingEvent event) {
+		ObjectIntIdentityMap<IBlockState> blockStateIDMap = GameData.getBlockStateIDMap();
+
+		int id = GameData.getBlockRegistry().getId(Blocks.snow_layer);
+
+		for (IBlockState state : blockSnowLayerReplacement.getBlockState().getValidStates()) {
+			blockStateIDMap.put(state, id << 4 | blockSnowLayerReplacement.getMetaFromState(state));
+		}
+
+		LOGGER.info("minecraft:snow_layer = {}", getSnowBlock().getClass());
+	}
+
+	private Block getSnowBlock() {
+		return GameRegistry.findBlock("minecraft", "snow_layer");
 	}
 }
