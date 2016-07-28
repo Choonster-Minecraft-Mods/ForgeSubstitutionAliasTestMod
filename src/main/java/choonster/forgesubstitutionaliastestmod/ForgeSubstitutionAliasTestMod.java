a88ddd04707c9375ee3d8a272f242308fe83245d
Choonster TheMage <Choonster.2010@gmail.com>
2016-07-29 04:25:58 +1000

    Reduce to minimal test case
    - Replace dirt instead of snow since it's the block used by FML's
    SubstitutionInjectionTest

--- src/main/java/choonster/forgesubstitutionaliastestmod/ForgeSubstitutionAliasTestMod.java
+++ src/main/java/choonster/forgesubstitutionaliastestmod/ForgeSubstitutionAliasTestMod.java
@@ -1,12 +1,9 @@
 package choonster.forgesubstitutionaliastestmod;
 
-import net.minecraft.client.renderer.ItemMeshDefinition;
-import net.minecraft.client.renderer.block.model.ModelBakery;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockDirt;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -20,58 +17,29 @@ public class ForgeSubstitutionAliasTestMod {
 	public static final String MODID = "forgesubstitutionaliastestmod";
 	public static final String NAME = "Forge Substitution Alias Test Mod";
 
-	private static final ResourceLocation SNOW_LAYER = new ResourceLocation("minecraft", "snow_layer");
-	private static final ResourceLocation STICK = new ResourceLocation("minecraft", "stick");
+	private static final ResourceLocation DIRT = new ResourceLocation("minecraft", "dirt");
 
 	private Logger LOGGER;
-	private BlockSnowLayerReplacement blockSnowLayerReplacement;
+	private Block blockDirtReplacement;
 
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 		LOGGER = event.getModLog();
 
-		blockSnowLayerReplacement = new BlockSnowLayerReplacement();
+		blockDirtReplacement = new BlockDirt() {
+		};
 
 		try {
-			GameRegistry.addSubstitutionAlias(SNOW_LAYER.toString(), GameRegistry.Type.BLOCK, blockSnowLayerReplacement);
-			GameRegistry.addSubstitutionAlias(SNOW_LAYER.toString(), GameRegistry.Type.ITEM, new ItemSnowLayerReplacement(blockSnowLayerReplacement));
-			LOGGER.info("Added substitution for {}", SNOW_LAYER);
+			GameRegistry.addSubstitutionAlias(DIRT.toString(), GameRegistry.Type.BLOCK, blockDirtReplacement);
+			LOGGER.info("Added substitution for {}", DIRT);
 		} catch (ExistingSubstitutionException e) {
-			LOGGER.error("Unable to add substitution alias for " + SNOW_LAYER, e);
-		}
-
-		final ItemStickReplacement itemStickReplacement = new ItemStickReplacement();
-		itemStickReplacement.setRegistryName(STICK);
-		try {
-			GameRegistry.addSubstitutionAlias(STICK.toString(), GameRegistry.Type.ITEM, itemStickReplacement);
-			LOGGER.info("Added substitution for {}", STICK);
-		} catch (ExistingSubstitutionException e) {
-			LOGGER.error("Unable to add substitution alias for " + STICK, e);
-		}
-
-		if (event.getSide().isClient()) {
-			final ModelResourceLocation modelResourceLocation = new ModelResourceLocation(SNOW_LAYER, "layers=1,enabled=true");
-
-			final Item itemSnowLayer = Item.getItemFromBlock(blockSnowLayerReplacement);
-			ModelBakery.registerItemVariants(itemSnowLayer, modelResourceLocation);
-			ModelLoader.setCustomMeshDefinition(itemSnowLayer, new ItemMeshDefinition() {
-				@Override
-				public ModelResourceLocation getModelLocation(ItemStack stack) {
-					return modelResourceLocation;
-				}
-			});
-
-			final ModelResourceLocation stickModel = new ModelResourceLocation(new ResourceLocation(MODID, "stick"), "inventory");
-			ModelLoader.setCustomModelResourceLocation(itemStickReplacement, 0, stickModel);
+			LOGGER.error("Unable to add substitution alias for " + DIRT, e);
 		}
 	}
 
 	@Mod.EventHandler
 	public void postInit(FMLPostInitializationEvent event) {
-		LOGGER.info("{} (Block Registry) = {}", SNOW_LAYER, ForgeRegistries.BLOCKS.getValue(SNOW_LAYER).getClass());
-		LOGGER.info("{} (Item Registry) = {}", SNOW_LAYER, ForgeRegistries.ITEMS.getValue(SNOW_LAYER).getClass());
-		LOGGER.info("{} (Item.getItemFromBlock) = {}", SNOW_LAYER, Item.getItemFromBlock(blockSnowLayerReplacement).getClass());
-
-		LOGGER.info("{} = {}", STICK, ForgeRegistries.ITEMS.getValue(STICK).getClass());
+		LOGGER.info("{} (Block Registry) = {}", DIRT, ForgeRegistries.BLOCKS.getValue(DIRT).getClass());
+		LOGGER.info("{} (Item.getItemFromBlock) = {}", DIRT, Item.getItemFromBlock(blockDirtReplacement).getClass());
 	}
 }
