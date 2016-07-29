3243f7f139955ad1b1c4a27e8786522e79622a60
Choonster TheMage <Choonster.2010@gmail.com>
2016-07-30 03:16:45 +1000

    Add ItemBlock substitution alias
    - Update Forge
    - Set the hardness, sound type, unlocalised name and registry name of
    the replacement Block

--- src/main/java/choonster/forgesubstitutionaliastestmod/ForgeSubstitutionAliasTestMod.java
+++ src/main/java/choonster/forgesubstitutionaliastestmod/ForgeSubstitutionAliasTestMod.java
@@ -1,8 +1,12 @@
 package choonster.forgesubstitutionaliastestmod;
 
+import com.google.common.base.Function;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockDirt;
+import net.minecraft.block.SoundType;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemMultiTexture;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
@@ -11,6 +15,8 @@ import net.minecraftforge.fml.common.registry.ExistingSubstitutionException;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import org.apache.logging.log4j.Logger;
+
+import javax.annotation.Nullable;
 
 @Mod(modid = ForgeSubstitutionAliasTestMod.MODID, name = ForgeSubstitutionAliasTestMod.NAME)
 public class ForgeSubstitutionAliasTestMod {
@@ -27,10 +33,28 @@ public class ForgeSubstitutionAliasTestMod {
 		LOGGER = event.getModLog();
 
 		blockDirtReplacement = new BlockDirt() {
+			{
+				setHardness(0.5F);
+				setSoundType(SoundType.GROUND);
+				setUnlocalizedName("dirt");
+				setRegistryName(DIRT);
+			}
 		};
 
 		try {
 			GameRegistry.addSubstitutionAlias(DIRT.toString(), GameRegistry.Type.BLOCK, blockDirtReplacement);
+
+			final ItemMultiTexture item = new ItemMultiTexture(blockDirtReplacement, blockDirtReplacement, new Function<ItemStack, String>() {
+				@Nullable
+				@Override
+				public String apply(@Nullable ItemStack stack) {
+					return BlockDirt.DirtType.byMetadata(stack.getMetadata()).getUnlocalizedName();
+				}
+			});
+
+			item.setRegistryName(DIRT);
+			GameRegistry.addSubstitutionAlias(DIRT.toString(), GameRegistry.Type.ITEM, item);
+
 			LOGGER.info("Added substitution for {}", DIRT);
 		} catch (ExistingSubstitutionException e) {
 			LOGGER.error("Unable to add substitution alias for " + DIRT, e);
